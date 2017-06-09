5f017269a6855c1ec8cc3e08eee52e6274ff290c
Paul Landes <landes@mailc.net>
2017-06-09 18:02:38 -0500

    package func

--- src/mk/dist.mk
+++ src/mk/dist.mk
@@ -11,9 +11,13 @@ DIST_BIN_DNAME=	bin
 DIST_BIN_DIR=	$(DIST_DIR)/$(DIST_BIN_DNAME)
 DIST_UJAR_REF=	$(ANRRES).jar
 DIST_UJAR_FILE=	$(DIST_DIR)/$(DIST_UJAR_REF)
+DIST_JAR_PROFS+=with-profile +appassem
 
 .PHONY: dist
 dist:		$(DIST_BIN_DIR)
+
+.PHONY: package
+package:	$(AS_DIR)
 
 .PHONY: distuber
 distuber:	uber
@@ -31,7 +35,7 @@ distinfo:
 app:		$(AS_DIR)
 
 $(AS_DIR):	$(POM) $(COMP_DEPS)
-	$(LEIN) with-profile +appassem jar
+	$(LEIN) $(DIST_JAR_PROFS) jar
 	mvn package appassembler:assemble
 
 $(DIST_BIN_DIR):	$(AS_DIR)
