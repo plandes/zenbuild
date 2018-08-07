94c4724b64e20bb6fd8aecd6573cbb1c89b09d2b
Paul Landes <landes@mailc.net>
2018-08-06 20:46:37 -0500

    add pypi deploy install script

---
+++ bin/py-install-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+PYPI_URLS="https://test.pypi.org/legacy/ https://upload.pypi.org/legacy/"
+
+for i in setuptools twine wheel keyring ; do
+    pip install -U $i
+done
+
+echo "yeah, you really have to provide password (each time); this stores it with keyring"
+for i in $PYPI_URLS ; do
+    python -m keyring set $i zensols
+done
