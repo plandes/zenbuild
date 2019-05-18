37b288b53c6597c91060cf2aed4a1afab1b39f15
Paul Landes <landes@mailc.net>
2019-05-18 07:37:57 -0500

    flexible input org readme file

--- src/mk/orgmode-markdown.mk
+++ src/mk/orgmode-markdown.mk
@@ -2,10 +2,12 @@
 
 OM_MD_FILE_NAME ?=	README.md
 
-OM_MD_EXPORT_EVAL =	$(subst README,$(OM_MD_FILE_NAME),$(subst FILE,$(CNT_SITE_NAME),"\
+OM_MD_ORG_NAME	?=	$(CNT_SITE_NAME).org
+
+OM_MD_EXPORT_EVAL =	$(subst README,$(OM_MD_FILE_NAME),$(subst FILE,$(OM_MD_ORG_NAME),"\
 (progn (load \"~/.emacs\")\
   (with-temp-buffer (org-mode)\
-  (find-file \"FILE.org\")\
+  (find-file \"FILE\")\
   (org-export-to-file 'gfm \"README\")))"))
 
 CNT_DEP_TARGS +=	$(OM_MD_FILE_NAME)
