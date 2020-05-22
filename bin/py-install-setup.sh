ea0167b72413ac4001544b5989ebcfdbc99781ba
Paul Landes <landes@mailc.net>
2020-05-22 00:24:37 -0500

    add sphinx deps to init install

--- bin/py-install-setup.sh
+++ bin/py-install-setup.sh
@@ -3,7 +3,7 @@
 PYPI_URLS="https://test.pypi.org/legacy/ https://upload.pypi.org/legacy/"
 
 echo "installing libraries for Python deployments"
-for i in setuptools twine wheel keyring ; do
+for i in setuptools twine wheel keyring sphinx sphinx-rtd-theme ; do
     pip install -U $i
 done
 
@@ -16,11 +16,6 @@ echo "yeah, you really have to provide password (each time); this stores it with
 for i in $PYPI_URLS ; do
     python -m keyring set $i zensols
 done
-
-# install certifications on OSX
-# if [[ "$OSTYPE" == "darwin"* ]] ; then
-#     /Applications/Python\ 3.6/Install\ Certificates.command
-# fi
 
 # needed for python utililty scripts
 pip install -r requirements.txt 
