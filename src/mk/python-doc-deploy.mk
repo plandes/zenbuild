9939379fb4bd7d1adc32e16aaa8c1ae647c831fa
Paul Landes <landes@mailc.net>
2020-11-21 16:02:34 -0600

    fix check mount dir to avoid remount; cond variables

--- src/mk/python-doc-deploy.mk
+++ src/mk/python-doc-deploy.mk
@@ -2,12 +2,14 @@
 ## PL 11/20/2020
 
 ## python doc
+# if provided, the mount is skipped if the directory is found
+CNT_MOUNT_CHECK_DIR ?=	$(CNT_INST_DIR)/$(GIT_PROJ)
 # the generated documentation directory
-CNT_SITE_DIR = 		$(GIT_DOC_SRC_DIR)
+CNT_SITE_DIR ?= 	$(GIT_DOC_SRC_DIR)
 # site build directory on package
-CNT_STAGE_DIR = 	$(PY_DOC_DIR)/stage
+CNT_STAGE_DIR ?= 	$(PY_DOC_DIR)/stage
 # this will be mounted via webdav
-CNT_DOC_URL =		$(CNT_SITE_SERV)/$(CNT_SITE_ROOT)
+CNT_DOC_URL ?=		$(CNT_SITE_SERV)/$(CNT_SITE_ROOT)
 # source directory to use for copy with rsync
 CNT_SRC_STAGE_DIR ?=	$(CNT_STAGE_DIR)/$(GIT_PROJ)
 # the path on the mounted volume path (CNT_INST_DIR) to rsync to
@@ -19,8 +21,17 @@ CNT_INST_DIR ?=		$(CNT_MNT_DIR)/$(CNT_SITE_ROOT)/$(CNT_DEPLOY_PATH)apidoc
 # destination directory, which is the name of the project
 CNT_DEPLOY_DEP_TARGS +=	pydocdeploymv
 
+# module config
+INFO_TARGETS +=		pydocdeployinfo
+
 ## includes
 include $(BUILD_MK_DIR)/content.mk
+
+
+.PHONY:			pydocdeployinfo
+pydocdeployinfo:
+			@echo "cnt-mount-check-dir: $(CNT_MOUNT_CHECK_DIR)"
+			@echo "cnt-deploy-path: $(CNT_DEPLOY_PATH)"
 
 
 .PHONY:			pydocdeploy
@@ -29,4 +40,7 @@ pydocdeploy:		pydochtml cntdeploy
 
 .PHONY:			pydocdeploymv
 pydocdeploymv:
+			@if [ -d "$(CNT_SRC_STAGE_DIR)" ] ; then \
+				rm -r $(CNT_SRC_STAGE_DIR) ; \
+			fi
 			mv $(CNT_STAGE_DIR)/html $(CNT_SRC_STAGE_DIR)
