771881342186d65acd1890b449d04c74b25b264c
Paul Landes <landes@mailc.net>
2021-11-13 12:49:29 -0600

    tex: phone targets for bibstract

--- src/mk/tex-bibstract.mk
+++ src/mk/tex-bibstract.mk
@@ -7,6 +7,13 @@ BIBSTRACT ?=		bibstract
 COMP_DEPS +=		$(BIB_FILE)
 ADD_CLEAN_ALL +=	$(BIB_FILE)
 
+.PHONY:			texbibstract
+texbibstract:		$(BIB_FILE)
+
+.PHONY:			texcleanbibstract
+texcleanbibstract:
+			rm -rf $(BIB_FILE)
+
 $(BIB_FILE):		$(BIB_MASTER_FILE)
 			@echo "running bibstract on $(BIB_FILE)..."
 			$(BIBSTRACT) export $(BIBSTRACT_TEX_PATH) > $(BIB_FILE)
