288d4fec20a73c3a646f94af906e97845dd639f7
Paul Landes <landes@mailc.net>
2021-12-10 20:35:10 -0600

    whitespace

--- src/mk/docker.mk
+++ src/mk/docker.mk
@@ -19,79 +19,79 @@ DOCKER_UP_DEPS ?=	dockerbuild
 
 INFO_TARGETS +=		dockerinfo
 
-.PHONY: dockerinfo
+.PHONY:		dockerinfo
 dockerinfo:
-	@echo "image-name: $(DOCKER_IMG_NAME)"
-	@echo "image: $(DOCKER_IMG)"
-	@echo "container: $(DOCKER_CONTAINER)"
-	@echo "docker-version: $(DOCKER_VERSION)"
+		@echo "image-name: $(DOCKER_IMG_NAME)"
+		@echo "image: $(DOCKER_IMG)"
+		@echo "container: $(DOCKER_CONTAINER)"
+		@echo "docker-version: $(DOCKER_VERSION)"
 
-.PHONY:	dockercheckver
+.PHONY:		dockercheckver
 dockercheckver:
-	$(eval DOCKER_VER_LAST_TAG := $(shell git tag -l | sort -V | tail -1 | sed 's/.//'))
-	@echo $(DOCKER_VER_LAST_TAG) =? $(DOCKER_VERSION) ; [ "$(DOCKER_VER_LAST_TAG)" == "$(DOCKER_VERSION)" ]
+		$(eval DOCKER_VER_LAST_TAG := $(shell git tag -l | sort -V | tail -1 | sed 's/.//'))
+		@echo $(DOCKER_VER_LAST_TAG) =? $(DOCKER_VERSION) ; [ "$(DOCKER_VER_LAST_TAG)" == "$(DOCKER_VERSION)" ]
 
-.PHONY: dockerbuild
+.PHONY:		dockerbuild
 dockerbuild:	$(DOCKER_BUILD_OBJS)
-	$(DOCKER_CMD) rmi $(DOCKER_IMG) || true
-	$(DOCKER_CMD) build $(DOCKER_BUILD_ARGS) -t $(DOCKER_IMG) $(DOCKER_PREFIX)
-	$(DOCKER_CMD) tag $(DOCKER_IMG) $(DOCKER_IMG):$(DOCKER_VERSION)
+		$(DOCKER_CMD) rmi $(DOCKER_IMG) || true
+		$(DOCKER_CMD) build $(DOCKER_BUILD_ARGS) -t $(DOCKER_IMG) $(DOCKER_PREFIX)
+		$(DOCKER_CMD) tag $(DOCKER_IMG) $(DOCKER_IMG):$(DOCKER_VERSION)
 
-.PHONY: dockerbuildnocache
+.PHONY: 	dockerbuildnocache
 dockerbuildnocache:	$(DOCKER_BUILD_OBJS)
-	$(DOCKER_CMD) rmi $(DOCKER_IMG) || true
-	$(DOCKER_CMD) build --no-cache -t $(DOCKER_IMG) .
-	$(DOCKER_CMD) tag $(DOCKER_IMG) $(DOCKER_IMG):$(DOCKER_VERSION)
+		$(DOCKER_CMD) rmi $(DOCKER_IMG) || true
+		$(DOCKER_CMD) build --no-cache -t $(DOCKER_IMG) .
+		$(DOCKER_CMD) tag $(DOCKER_IMG) $(DOCKER_IMG):$(DOCKER_VERSION)
 
-.PHONY:	dockerpush
+.PHONY:		dockerpush
 dockerpush:	dockerbuild
-	$(DOCKER_CMD) push $(DOCKER_IMG)
+		$(DOCKER_CMD) push $(DOCKER_IMG)
 
-.PHONY:	dockerinstall
+.PHONY:		dockerinstall
 dockerinstall:	dockercheckver
-	make DOCKER_VERSION=$(GIT_VER) dockerbuild
+		make DOCKER_VERSION=$(GIT_VER) dockerbuild
 
-.PHONY: dockerrm
+.PHONY:		dockerrm
 dockerrm:	dockerrmi
-	@echo "remember to shut down the image first"
-	$(DOCKER_CMD) images | grep $(DOCKER_IMG) | awk '{print $$2}' | xargs -i{} $(DOCKER_CMD) rmi $(DOCKER_IMG):{}
+		@echo "remember to shut down the image first"
+		$(DOCKER_CMD) images | grep $(DOCKER_IMG) | awk '{print $$2}' | xargs -i{} $(DOCKER_CMD) rmi $(DOCKER_IMG):{}
 
 # remove all "orphan" images
-.PHONY:	dockerrmi
+.PHONY:		dockerrmi
 dockerrmi:
-	$(DOCKER_CMD) images | grep '<none>' | awk '{print $$3}' | xargs docker rmi || true
+		$(DOCKER_CMD) images | grep '<none>' | awk '{print $$3}' | xargs docker rmi || true
 
-.PHONY:	dockerrmzombie
+.PHONY:		dockerrmzombie
 dockerrmzombie:
-	for i in $$($(DOCKER_CMD) ps -a --format '{{.Names}} {{.Status}}' | \
-		grep Exited | \
-		awk '{print $$1}') ; do \
-		$(DOCKER_CMD) rm $$i ; \
-	done
+		for i in $$($(DOCKER_CMD) ps -a --format '{{.Names}} {{.Status}}' | \
+			grep Exited | \
+			awk '{print $$1}') ; do \
+			$(DOCKER_CMD) rm $$i ; \
+		done
 
-.PHONY:	dockerup
+.PHONY:		dockerup
 dockerup:	$(DOCKER_UP_DEPS)
-	$(DOCKER_CMP_CMD) up -d
+		$(DOCKER_CMP_CMD) up -d
 
-.PHONY:	dockerdown
+.PHONY:		dockerdown
 dockerdown:
-	$(DOCKER_CMP_CMD) down
+		$(DOCKER_CMP_CMD) down
 
-.PHONY:	dockerrestart
-dockerrestart:	dockerdown dockerup
+.PHONY:		dockerrestart
+dockerrestart:		dockerdown dockerup
 
-.PHONY:	dockerps
+.PHONY:		dockerps
 dockerps:
-	$(DOCKER_CMD) ps -a
+		$(DOCKER_CMD) ps -a
 
-.PHONY: dockerlogin
+.PHONY:		dockerlogin
 dockerlogin:
-	$(DOCKER_CMD) exec -it $(DOCKER_CONTAINER) bash
+		$(DOCKER_CMD) exec -it $(DOCKER_CONTAINER) bash
 
 .PHONY: dockerlog
 dockerlog:
-	$(DOCKER_CMD) logs $(DOCKER_CONTAINER) -f
+		$(DOCKER_CMD) logs $(DOCKER_CONTAINER) -f
 
-.PHONY:	dockerstatus
+.PHONY:		dockerstatus
 dockerstatus:
-	$(DOCKER_CMP_CMD) ps
+		$(DOCKER_CMP_CMD) ps
