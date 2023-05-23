a5d5c3a637ed1b9c35fa1050ccebbed7c46464b4
Paul Landes <landes@mailc.net>
2023-05-22 22:37:17 -0500

    add common lisp build automation

---
+++ src/proj/cl.mk
@@ -0,0 +1,25 @@
+# ANSI common lisp
+# Created: 5/22/2023
+
+# entry application
+CL_PROTO_APP ?= 	./main.ros
+
+# run arguments
+CL_RUN_ARGS ?=		run
+
+
+# info
+INFO_TARGETS +=		clinfo
+
+
+.PHONY:			test
+test:
+			$(CL_PROTO_APP) test
+
+.PHONY:			run
+run:
+			$(CL_PROTO_APP) $(CL_RUN_ARGS)
+
+.PHONY:			clinfo
+clinfo:
+			@echo "proto-app: $(PROTO_APP)"
