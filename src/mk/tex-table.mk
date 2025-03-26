e3861b65ee917e2a76424c6183251f5c22d956d1
Paul Landes <landes@mailc.net>
2025-03-26 08:07:45 -0500

    latex: datdesc python path for imports and better relative files

--- src/mk/tex-table.mk
+++ src/mk/tex-table.mk
@@ -4,34 +4,45 @@
 # PROJ_LOCAL_MODULES= tex-table
 
 
+## Module
+#
 # the script to generate the latex table
 TEX_TAB_BIN ?=		datdesc
+# directory to run the program assumes the parent Python path for imports
+TEX_TAB_WD ?=		$(abspath ..)
 # where JSON table definitions live
 TEX_TAB_DIR ?=		$(TEX_CONF_DIR)
 # all table definitions
 TEX_TAB_DEFS +=		$(wildcard $(TEX_TAB_DIR)/*-table.yml)
 TEX_TAB_STYS =		$(addprefix $(TEX_LAT_PATH)/,$(notdir $(patsubst %.yml,%.sty,$(TEX_TAB_DEFS))))
 
-# build
+
+## Build
+#
 INFO_TARGETS +=		textabinfo
 TEX_PRE_COMP_DEPS +=	$(TEX_TAB_STYS)
 
 
+## Targets
+#
+# build info
 .PHONY:		textabinfo
 textabinfo:
 		@echo "tex-tab-defs: $(TEX_TAB_DEFS)"
 		@echo "tex-tab-stys: $(TEX_TAB_STYS)"
 
+# run the program to generate the tables
 .PHONY:		textab
 textab:
-		@echo "creating $(TEX_TAB_STYS)"
-		$(TEX_TAB_BIN) table $(TEX_TAB_DIR) $(TEX_LAT_PATH)
+		@echo "creating $(TEX_TAB_STYS) in $(TEX_TAB_WD)"
+		( cd $(TEX_TAB_WD) ; \
+		   PYTHONPATH='.' \
+		   $(TEX_TAB_BIN) table $(TEX_TAB_DIR) $(TEX_LAT_PATH) )
 
+# convenience: run the program, compile the PDF, then display it
 .PHONY:		textabshow
-textabshow:
-		@echo "creating $(TEX_TAB_STYS)"
-		$(TEX_TAB_BIN) table $(TEX_TAB_DIR) $(TEX_LAT_PATH)
-		make show
+textabshow:	textab show
 
+# generate a new LaTeX .sty file for every *-table.yml found
 %-table.sty:	$(TEX_TAB_DIR)/$(@F) $(TEX_MTARG_FILE)
 		make textab
