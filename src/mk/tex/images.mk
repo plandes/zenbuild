9fa05cb6429ca84dfda96dd9b08a1f30f3f07d4f
Paul Landes <landes@mailc.net>
2026-05-22 10:29:00 +0200

    latex: cleanup

--- src/mk/tex/images.mk
+++ src/mk/tex/images.mk
@@ -8,20 +8,12 @@
 TEX_IMGS_SRC =		$(wildcard $(TEX_IMG_DIR)/*.eps) \
 			$(wildcard $(TEX_IMG_DIR)/*.png) \
 			$(wildcard $(TEX_IMG_DIR)/*.jpg)
-#TEX_IMGS_DST +=		$(patsubst $(TEX_IMG_DIR)/%,$(TEX_LAT_PATH)/%,$(TEX_IMGS_SRC))
-#TEX_PRE_COMP_DEPS +=	$(TEX_IMGS_DST)
 TEX_PRE_COMP_DEPS +=	teximagescopy
 
 
 ## Build
 #
-#TEX_INIT_RUN =		1
 INFO_TARGETS +=		teximagesinfo
-
-
-# $(foreach s,$(TEX_IMGS_SRC), \
-#   $(eval $(patsubst $(TEX_IMG_DIR)/%,$(TEX_LAT_PATH)/%,$(s)): $(s)) \
-# )
 
 
 ## Targets
@@ -38,16 +30,3 @@ teximagescopy:
 				echo "copy image: $$f -> $$dst"; \
 				install -m 644 "$$f" "$$dst"; \
 			done
-
-
-# $(TEX_LAT_PATH)/%.eps: $(TEX_IMG_DIR)/%.eps
-# 	@echo "copy image: $< $@"
-# 	cp $< $@
-
-# $(TEX_LAT_PATH)/%.png: $(TEX_IMG_DIR)/%.png
-# 	@echo "copy image: $< $@"
-# 	cp $< $@
-
-# $(TEX_LAT_PATH)/%.jpg: $(TEX_IMG_DIR)/%.jpg
-# 	@echo "copy image: $< $@"
-# 	cp $< $@
